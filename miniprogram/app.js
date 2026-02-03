e5b3f5ce053c95dc3a750433f8400560a41f9e8f
furuilin <furuilin@hasng.cn>
2026-02-03 17:12:54 +0800

    提交所有代码

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,7 +2,7 @@ App({
   globalData: {
     teamId: null,
     teamName: null,
-    apiBaseUrl: 'http://1.15.149.170:3002/api' // Updated to LAN IP
+    apiBaseUrl: 'http://192.168.43.91:3002/api' // Updated to LAN IP
   },
   onLaunch() {
     // Check local storage for login
